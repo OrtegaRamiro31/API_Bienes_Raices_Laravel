f73a6cd5ad2f6c91413cf8e90e6911f2d47d7e87
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-23 14:31:03 -0600

    Added fields for Property table

--- database/migrations/2023_06_23_202525_create_properties_table.php
+++ database/migrations/2023_06_23_202525_create_properties_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->double('price');
+            $table->text('description');
             $table->timestamps();
         });
     }
