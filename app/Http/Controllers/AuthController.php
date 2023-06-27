95b2d7fae735ef7ecfed34fef84ebd372293c316
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-26 19:02:08 -0600

    Implemented logout user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,4 +46,13 @@ class AuthController extends Controller
             'user' => $user,
         ];
     }
+
+    public function logout( Request $request ) {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return [
+            'user' => null,
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,12 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    // Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('/properties', PropertyController::class);
 // Route::put('/properties', [PropertyController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
